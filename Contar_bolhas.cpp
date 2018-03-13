72a3e804da3fe4d9b7060d6884e663732b8f0f1b
alex7alves <alex7alves10@hotmail.com>
2018-03-13 15:34:34 -0300

    Finalizado para objetos com 1 buraco

--- Contar_bolhas.cpp
+++ Contar_bolhas.cpp
@@ -1,8 +1,8 @@
 
 /*
-	Programa para contar bolhas e os  buracos dentro das bolhas
+  Programa para contar bolhas e os  buracos dentro das bolhas
 
-	Autor : Alex Alves
+  Autor : Alex Alves
 */
 
 #include <iostream>
@@ -12,69 +12,77 @@ using namespace cv;
 
 
 CvPoint ponto(int i, int j){
-	CvPoint p;
-	p.x=j;
-	p.y=i;
-	return p;
+
+    CvPoint p;
+    p.x=j;
+    p.y=i;
+    return p;
 }
 
 void Bordas( Mat bolha, int passo_linha,int passo_coluna)
 {
-	CvPoint p;
-	for(int i=0; i<bolha.rows; i = i+passo_linha){
-    	for(int j=0; j<bolha.cols; j= j+passo_coluna){
-    		if(bolha.at<uchar>(i,j)==255){
-    			p=ponto(i,j);
-    			floodFill(bolha,p,0);
-    		}
-		}
-  	}
+
+    CvPoint p;
+    for(int i=0; i<bolha.rows; i = i+passo_linha){
+        for(int j=0; j<bolha.cols; j= j+passo_coluna){
+            if(bolha.at<uchar>(i,j)==255){
+                p=ponto(i,j);
+                floodFill(bolha,p,0);
+            }
+        }
+    }
 }
 
-
-
-
 int main(int argc, char** argv){
-  Mat image;
 
-  int nobjects;
-  
-  CvPoint p;
-  image = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
-  Mat bolha(image.rows,image.cols,image.type());
-  if(!image.data){
-    std::cout << "imagem nao carregou corretamente\n";
-    return(-1);
-  }
-  p = ponto(0,0);
-  // copiando imagem original
-  for(int i=0; i<image.rows; i++){
-    for(int j=0; j<image.cols; j++){
-    	bolha.at<uchar>(i,j) = image.at<uchar>(i,j);	
-	}
-  }
-  // Retirando bolhas que tocam as bordas nas linhas
-  Bordas(bolha,bolha.rows-1,1);
-  // Retirando bolhas que tocam as bordas nas colunas
-  Bordas(bolha,1,bolha.cols-1);
+    Mat image;
 
- 
-  // busca objetos com buracos presentes
-/*  nobjects=0;
-  for(int i=0; i<image.rows; i++){
-    for(int j=0; j<image.cols; j++){
-      if(image.at<uchar>(i,j) == 255){
-		// achou um objeto
-		nobjects++;
-		p.x=j;
-		p.y=i;
-		floodFill(image,p,nobjects);
-	  }
-	}
-  }*/
-  imshow("image", image);
-  imshow("Modificado",bolha);
-  //imwrite("labeling.png", image);
-  waitKey();
-  return 0;
+    int nobjects=0,nburacos=0;
+    CvPoint p;
+    image = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+    Mat bolha(image.rows,image.cols,image.type());
+    if(!image.data){
+        std::cout << "imagem nao carregou corretamente\n";
+        return(-1);
+    }
+    // copiando imagem original
+    for(int i=0; i<image.rows; i++){
+        for(int j=0; j<image.cols; j++){
+            bolha.at<uchar>(i,j) = image.at<uchar>(i,j);  
+        }
+    }
+    // Retirando bolhas que tocam as bordas nas linhas
+    Bordas(bolha,bolha.rows-1,1);
+    // Retirando bolhas que tocam as bordas nas colunas
+    Bordas(bolha,1,bolha.cols-1);
+
+     
+    // busca objetos  presentes
+    for(int i=0; i<bolha.rows; i++){
+        for(int j=0; j<bolha.cols; j++){
+            if(bolha.at<uchar>(i,j)==255){
+                nobjects++;
+                p=ponto(i,j);
+                floodFill(bolha,p,100);
+            }
+        }
+    }
+    p=ponto(0,0);
+    floodFill(bolha,p,50);
+    // busca objetos com buracos presentes
+    for(int i=0; i<bolha.rows; i++){
+        for(int j=0; j<bolha.cols; j++){
+            if(bolha.at<uchar>(i,j)==0){
+                nburacos++;
+                p=ponto(i,j);
+                floodFill(bolha,p,255);
+            }
+        }
+    }
+    printf("%d objetos foram encontrados, sendo que %d tem buracos\n",nobjects,nburacos); 
+    imshow("image", image);
+    imshow("Modificado",bolha);
+    //imwrite("labeling.png", image);
+    waitKey();
+    return 0;
 }
