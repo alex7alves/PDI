d19947cf0df2b43b5819df0683829217f928648d
alex7alves <alex7alves10@hotmail.com>
2018-05-16 20:49:33 -0300

    Salvando imagens e organizando funções

--- arteCanny.cpp
+++ arteCanny.cpp
@@ -21,20 +21,8 @@ using namespace std;
 using namespace cv;
 
 
-
-vector<int> AmostrarCentros(vector<int> y, int passo)
-{
-  for(uint i=0; i<y.size(); i++){
-    y[i]= y[i]*passo+passo/2;
-  }
-  return y;
-}
-
-void Circulo(Mat im, Mat art, Vec3b cor, int raio,int x,int y )
-{
-  cor = im.at<Vec3b>(x,y);
-  circle(art,cv::Point(y,x),raio,CV_RGB(cor[2],cor[1],cor[0]),-1, CV_AA);
-}
+vector<int> AmostrarCentros(vector<int> y, int passo);
+void Circulo(Mat im, Mat art, Vec3b cor, int raio,int x,int y );
 
 
 int main(int argc, char** argv){
@@ -45,7 +33,7 @@ int main(int argc, char** argv){
   int fator =60;
 
   int x, y;
-  int STEP=5,JITTER=3,RAIO=4; 
+  int STEP=5,JITTER=3,RAIO=3; 
   image = imread(argv[1]);
   Mat  arte(image.rows,image.cols,image.type());
   Vec3b cor;
@@ -78,27 +66,38 @@ int main(int argc, char** argv){
       Circulo(image,arte,cor,RAIO,x,y);
     }
   }
+  imwrite("pontos.jpg",arte);
 
   cvtColor(arte, cinza, CV_BGR2GRAY);
   GaussianBlur(cinza, borrar, Size(5,5), 25, 25);
   Canny(borrar, bordas,fator,3*fator);
+  imwrite("canny.jpg",bordas);
 
   for(int i=0;i<arte.rows;i++){
     for(int j=0;j<arte.cols;j++){
-        if(bordas.at<uchar>(i,j)==255){
-          Circulo(arte,arte,cor,1,i,j);
-          }
-        }
-
+      if(bordas.at<uchar>(i,j)==255){
+        Circulo(arte,arte,cor,2,i,j);
+      }
     }
+  }
 
-  
-
-
-  //imwrite("pontos.jpg", points);
- // imshow("pontos", points);
-  imshow("pontos",arte);
+  imwrite("arte.jpg",arte);
+  imshow("arte",arte);
   waitKey();
   return 0;
     
+}
+
+vector<int> AmostrarCentros(vector<int> y, int passo)
+{
+  for(uint i=0; i<y.size(); i++){
+    y[i]= y[i]*passo+passo/2;
+  }
+  return y;
+}
+
+void Circulo(Mat im, Mat art, Vec3b cor, int raio,int x,int y )
+{
+  cor = im.at<Vec3b>(x,y);
+  circle(art,cv::Point(y,x),raio,CV_RGB(cor[2],cor[1],cor[0]),-1, CV_AA);
 }
